0aa85e9356fc063d2f0549410be1566f777c3f46
Jack <jackshaug30@outlook.com>
2022-04-13 12:44:21 +0100

    Started on calendar. Authorisation error with connecting to google calendar API. Might need to use different API.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
 
   #get 'ptchat', to: ''
   get 'ezmuscle', to: 'ezmuscle#ezmuscle'
+
   get 'calendar', to: 'calendar#calendar'
+  get '/redirect', to: 'calendar#redirect', as: 'redirect'
+  get '/callback', to: 'calendar#callback', as: 'callback'
+
   get 'gymfinder', to: 'gymfinder#gymfinder'
   get 'contact', to: 'home#contact'
   post 'request_contact', to: 'home#request_contact'

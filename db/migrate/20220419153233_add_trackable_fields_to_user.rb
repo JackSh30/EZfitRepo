3cb0303f6ca4c9f026e3eb319c19cb50c6c49d0a
JackSh30 <js02220@surrey.ac.uk>
2022-05-10 13:42:11 +0100

    Converted database from sqlite to postgres for use with heroku

--- db/migrate/20220419153233_add_trackable_fields_to_user.rb
+++ db/migrate/20220419153233_add_trackable_fields_to_user.rb
@@ -4,8 +4,8 @@ class AddTrackableFieldsToUser < ActiveRecord::Migration[5.2]
        t.integer   :sign_in_count, default: 0, null: false
        t.datetime  :current_sign_in_at
        t.datetime  :last_sign_in_at
-       t.string    :current_sign_in_ip
-       t.string    :last_sign_in_ip
+       t.text    :current_sign_in_ip
+       t.text    :last_sign_in_ip
     end
   end
 end
